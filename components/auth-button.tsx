9cb665376b3b51051a0787b201ab9727211dbcb0
jianglei <jlsky519@163.com>
2025-09-18 10:17:04 +0800

    feat: init

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -18,12 +18,12 @@ export async function AuthButton() {
     </div>
   ) : (
     <div className="flex gap-2">
-      <Button asChild size="sm" variant={"outline"}>
-        <Link href="/auth/login">Sign in</Link>
-      </Button>
       <Button asChild size="sm" variant={"default"}>
-        <Link href="/auth/sign-up">Sign up</Link>
+        <Link href="/auth/login">查看后台统计</Link>
       </Button>
+      {/* <Button asChild size="sm" variant={"default"}>
+        <Link href="/auth/sign-up">Sign up</Link>
+      </Button> */}
     </div>
   );
 }

9339498d03a3183317340d31ae71580a155be915
jianglei <jlsky519@163.com>
2025-09-20 21:28:52 +0800

    fix: 修改类型错误

--- app/protected/[id]/page.tsx
+++ app/protected/[id]/page.tsx
@@ -4,14 +4,14 @@ import QuestionsResult from "@/components/questionsResult";
 export default async function ProtectedPage({
   params,
 }: {
-  params: { id: string };
+  params: { id: string | string[] | undefined };
 }) {
-  console.log("[ id ] >", params.id);
+  const id = await params.id;
   return (
     <div className="flex w-full h-[calc(100vh-8rem)] min-h-0">
       {/* 左侧：问卷内容 */}
       <section className="flex-1 min-h-0 overflow-y-auto ">
-        <QuestionsResult id={params.id} />
+        <QuestionsResult id={id} />
       </section>
       {/* 右侧：问卷列表 */}
       <aside className="w-80 max-w-xs min-w-[200px] border-l pl-4 h-full flex flex-col">
